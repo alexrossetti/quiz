930a8b845a18f38fa1bb127e5fb9ec26695c261c
Alex Rossetti <alexjrossetti@gmail.com>
2021-12-16 14:01:10 +0000

    add react router, setup basic routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Themes, GlobalStyles } from "./styles.js";
 
 export default function App() {
   return (
     <ThemeProvider theme={Themes["default"]}>
-      <GlobalStyles />
-      app
+      <BrowserRouter>
+        <GlobalStyles />
+        <Routes>
+          <Route index element={<h1>Home</h1>} />
+          <Route path="/quiz">
+            <Route path=":id" element={<h2>quiz</h2>} />
+          </Route>
+          <Route path="/results" element={<h1>Results</h1>} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
