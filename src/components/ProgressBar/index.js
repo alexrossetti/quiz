43b02a514993619f5d0e944525576ab9387a502a
Alex Rossetti <alexjrossetti@gmail.com>
2021-12-18 12:07:17 +0000

    add lines to progress bar

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -6,7 +6,6 @@ import { useResponses } from "../../contexts/ResponsesContext";
 export default function ProgressBar() {
 	const { responses } = useResponses();
 	const { questions } = useQuestions();
-
 	const resLength = responses.length;
 
 	return (
@@ -15,7 +14,14 @@ export default function ProgressBar() {
 				const isAnswered = index <= resLength - 1;
 				const isCorrect = isAnswered ? responses[index].isCorrect : null;
 
-				return <Dot isAnswered={isAnswered} isCorrect={isCorrect} />;
+				return (
+					<React.Fragment key={index}>
+						<Dot isAnswered={isAnswered} isCorrect={isCorrect} />
+						{index !== questions.length - 1 && (
+							<Line isAnswered={isAnswered} isCorrect={isCorrect} />
+						)}
+					</React.Fragment>
+				);
 			})}
 		</Wrapper>
 	);
@@ -33,9 +39,21 @@ const Dot = styled.div`
 	height: 20px;
 	border-radius: 50%;
 	background: ${(props) =>
-		!props.isAnswered ? "blue" : props.isCorrect ? "green" : "red"};
+		!props.isAnswered ? "black" : props.isCorrect ? "green" : "red"};
+	opacity: ${(props) => !props.isAnswered && "0.6"};
 
 	&:not(:last-child) {
 		margin-right: 5px;
 	}
+`;
+
+const Line = styled.div`
+	width: 15px;
+	height: 2px;
+	background: #333;
+	border-radius: 5px;
+	margin-right: 5px;
+	background: ${(props) =>
+		!props.isAnswered ? "black" : props.isCorrect ? "green" : "red"};
+	opacity: ${(props) => !props.isAnswered && "0.6"};
 `;
