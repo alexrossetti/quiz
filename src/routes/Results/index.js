bdade8ea77ddd477b6739a72f0257d17871791f1
Alex Rossetti <alexjrossetti@gmail.com>
2021-12-21 12:41:09 +0000

    show score on results page, add play again button

--- src/routes/Results/index.js
+++ src/routes/Results/index.js
@@ -1,12 +1,18 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import Header from "../../components/Header";
 import ProgressBar from "../../components/ProgressBar";
 import { useResponses } from "../../contexts/ResponsesContext";
 
 export default function Results() {
 	const navigate = useNavigate();
-	const { responses } = useResponses();
+	const { responses, setResponses } = useResponses();
+
+	const correctAnswers =
+		responses.length > 0
+			? responses.filter((res) => res.isCorrect).length
+			: null;
 
 	useEffect(() => {
 		if (!responses || responses.length === 0) {
@@ -19,8 +25,41 @@ export default function Results() {
 			<Header>
 				<ProgressBar />
 			</Header>
-			<h1>Results</h1>
-			<button onClick={() => navigate("/")}>Back to start</button>
+			<Content>
+				<h1>
+					You scored <br />
+					{correctAnswers} / {responses.length}
+				</h1>
+				<button onClick={() => navigate("/")}>Back to start</button>
+				<button
+					onClick={() => {
+						setResponses([]);
+						navigate("/quiz/0");
+					}}
+				>
+					Play again
+				</button>
+			</Content>
 		</>
 	);
 }
+
+const Content = styled.div`
+	padding: 10px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	text-align: center;
+
+	h1 {
+		font-size: 1.75rem;
+		line-height: 1.825rem;
+		margin: 30px 0px;
+	}
+
+	button {
+		&:not(:last-child) {
+			margin-bottom: 10px;
+		}
+	}
+`;
